7127ff10318134dc34e0a7c01149983ba05ffaa0
daecazu <daecazu@gmail.com>
2016-01-28 13:33:32 -0500

    adding data with forms

--- client/templates/resolutions/resolutions_list.js
+++ client/templates/resolutions/resolutions_list.js
@@ -3,3 +3,15 @@ Template.resolutionsList.helpers({
     return Resolutions.find();
   }
 });
+Template.resolutionsList.events({
+	'submit .new-resolution': function(event){
+		var title = event.target.title.value;//capturamos el valor del formulario como un evento
+		Resolutions.insert({ //insertamos titulo y fecha en la base de datos
+			title : title,
+			createdAt: new Date()
+		});
+		event.target.title.value =""; //sirve para limpiar el formulario
+
+		return false; //para que no se refresque
+	}
+});
